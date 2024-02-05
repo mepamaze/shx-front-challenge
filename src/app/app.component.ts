4354712dfeaa06c36b37e48841a3a100f61448ff
breno.rodrigues <87094994+shx-selecao@users.noreply.github.com>
2024-02-05 13:23:16 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Cotacao } from './cotacao';
+import { CotacaoDolarService } from './cotacaodolar.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  cotacaoAtual = 0;
+  cotacaoPorPeriodoLista: Cotacao[] = [];
+
+  constructor(
+    private cotacaoDolarService: CotacaoDolarService,
+    private dateFormat: DatePipe
+  ) {}
+
+  public getCotacaoPorPeriodo(
+    dataInicialString: string,
+    dataFinalString: string
+  ): void {
+    this.cotacaoPorPeriodoLista = [];
+
+    const dataInicial = this.dateFormat.transform(dataInicialString, "MM-dd-yyyy") || '';
+    const dataFinal = this.dateFormat.transform(dataFinalString, "MM-dd-yyyy") || '';
+
+    if (dataInicial && dataFinal) {
+      this.cotacaoDolarService.getCotacaoPorPeriodoFront(dataInicial, dataFinal).subscribe(cotacoes => {
+        this.cotacaoPorPeriodoLista = cotacoes;
+      })
+    } else {
+      //ALERTA DE ERRO POÍS DATA INICIAL E FINAL SÃO OBRIGATÓRIAS
+    }
+  }
+
+  ngOnInit() {
+    this.cotacaoDolarService.getCotacaoAtual().subscribe(cotacao => {
+      this.cotacaoAtual = cotacao;
+    })
+  }
+}
---
+++ src/app/cotacaodolar.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cotacao } from './cotacao';
+
+@Injectable({ providedIn: 'root' })
+export class CotacaoDolarService {
+  private apiServerUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  public getCotacaoAtual(): Observable<number> {
+    return this.http.get<any>(`${this.apiServerUrl}/moeda/atual`);
+  }
+
+  public getCotacaoPorPeriodoFront(
+    dataInicial: string,
+    dataFinal: string
+  ): Observable<Cotacao[]> {
+    return this.http.get<Cotacao[]>(`${this.apiServerUrl}/moeda/${dataInicial}&${dataFinal}`);
+  }
+}
